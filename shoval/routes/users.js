b666a156af7c10b94ef25a3929d5e368c9991433
Dorkes-PC\Dorkes <dor362@gmail.com>
2016-08-28 16:27:50 +0300

    Check the commit

--- shoval/routes/users.js
+++ shoval/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello world');
+  res.send('Hello users12');
 });
 
 module.exports = router;
